e553e88c40fcc2319cc50be6db21b93cbaf5d6ba
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-12-03 13:00:26 +0200

    add task1 	new file:   task1/dynamic_methods.rb

---
+++ task1/dynamic_methods.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# DynamicMethods example
+class DynamicMethods
+	def method_missing(method_name, *args, &block)
+		if method_name.to_s.start_with?('say_')
+			action = method_name.to_s.split('_', 2).last
+			puts "I say #{action.capitalize}!"
+		else
+			super # Викликає стандартну поведінку, якщо метод не оброблений
+		end
+	end
+end
+
+obj = DynamicMethods.new
+obj.say_hello # => "I say Hello!"
+obj.say_goodbye # => "I say Goodbye!"
+obj.unknown_method # => NoMethodError
